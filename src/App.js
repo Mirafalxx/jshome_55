616b5f16d94b5ff5a7e6d2d55cd1e01761301631
maxi.suhanov@gmail.com <maxi.suhanov@gmail.com>
2020-12-24 21:18:54 +0300

    fix sum counter

--- src/App.js
+++ src/App.js
@@ -9,13 +9,58 @@ const INGREDIENTS = [
 ];
 const App = () => {
   const [ingredients, setIngredients] = useState([
-    { name: "Meat", count: 0 },
-    { name: "Cheese", count: 0 },
-    { name: "Salad", count: 0 },
-    { name: "Bacon", count: 0 },
+    { name: "Meat", count: 0, price: 50 },
+    { name: "Cheese", count: 0, price: 20 },
+    { name: "Salad", count: 0, price: 5 },
+    { name: "Bacon", count: 0, price: 30 },
   ]);
-  const [price, setPrice] = useState(0);
-  return <div className="App"></div>;
+  const [burgerPrice, setBurgerPrice] = useState("");
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const createBurger = (index) => {
+    const copyIngredients = [...ingredients];
+    copyIngredients[index].count++;
+    let indexPrice = copyIngredients[index].price;
+    // console.log(indexPrice);
+    // let sum = [...burgerPrice];
+    let sum = copyIngredients[index].count * copyIngredients[index].price;
+    // let copyPrice = [...burgerPrice];
+    let copyPrice = [];
+    copyPrice.push(sum);
+    console.log(copyPrice);
+    // let totalSum = copyPrice.reduce(reducer);
+    // console.log(totalSum);
+    // setBurgerPrice(copyPrice);
+    // setBurgerPrice();
+
+    setIngredients(copyIngredients);
+  };
+  const deleteIngredient = (index) => {
+    const copyIngredients = [...ingredients];
+    copyIngredients[index].count = 0;
+    setIngredients(copyIngredients);
+  };
+  return (
+    <div className="App">
+      <div className="ingredientsForm">
+        {ingredients.map((elem, index) => {
+          return (
+            <div className="test" key={index}>
+              <button
+                className="btn deleteBtn"
+                onClick={() => createBurger(index)}
+              >
+                &times;
+              </button>
+              <h2>{elem.name}</h2>
+              <b>{elem.count}</b>
+              <button onClick={() => deleteIngredient(index)}>del</button>
+            </div>
+          );
+        })}
+      </div>
+      <h2>{burgerPrice}</h2>
+    </div>
+  );
 };
 
 export default App;
